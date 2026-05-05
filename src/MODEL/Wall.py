a4f94c303105bf3b091c774c265e3b420ef3c242
FiPippo <filippoprencipe0315@gmail.com>
2026-05-05 22:15:44 +0200

    MODEL: Implementata classe Wall e il suo costruttore

---
+++ src/MODEL/Wall.py
@@ -0,0 +1,21 @@
+import Cell
+
+
+class Wall:
+    """Questa classe rappresenta un muro all'interno del gioco "Quoridor".
+
+    Scopo e responsabilità della classe: Memorizzare la posizione iniziale del muro e il
+    suo orientamento (che può essere orizzontale oppure verticale).
+
+    """
+
+    def __init__(self, start_cell: 'Cell', orientation: str):
+        """Inizializza un nuovo muro.
+
+        Args:
+            start_cell (Cell): _description_
+            orientation (str): _description_
+
+        """
+        self._start_cell = start_cell
+        self._orientation = orientation

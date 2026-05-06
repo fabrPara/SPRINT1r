1ca00bafd24eb5f870ea9013daced90346b797c4
Federico <federicofasciano@MacBook-Air-di-Federico.local>
2026-05-06 16:57:12 +0200

    CreazioneClasseQuoridorGame

---
+++ src/MODEL/QuoridorGame.py
@@ -0,0 +1,34 @@
+from .Board import Board
+from .Cell import Cell
+from .Player import Player
+
+##importare la classe Exeption per gli tutti gli errori del gioco 
+
+
+class QuoridorGame:
+    """Classe principale per il gioco Quoridor.
+    
+    Gestisce la logica del tabellone, i giocatori e il loro turno.
+    Fornisce metodi per muovere i giocatori, posizionare muri, verificare il vincitore.
+    """
+    
+    def __init__(self):
+        """Inizializza una nuova partita di Quoridor.
+
+        Args:
+            Board: classe che rappresenta il tabellone di gioco
+            Player: classe che rappresenta un giocatore
+            Cell: classe che rappresenta una cella del tabellone
+
+        """
+        self._board=Board()   #crea il tabellone
+        
+        # crea i giocatori
+        p1_start=Cell(4, 0)  # posizione iniziale del giocatore 1
+        p2_start=Cell(4, 8)  # posizione iniziale del giocatore 2
+        p1=[Player(player_id=1,start_pos=p1_start,target_row=8)]
+        p2=[Player(player_id=2,start_pos=p2_start,target_row=0)]
+        self._players=[p1,p2]
+
+        self._current_turn=0 ##indice del giocatore che deve muovere (1 o 2)
+        self._winner=None
